859ca3eb76776c55eed8920673ff14ce9a11f603
Emanuel Facundo <38675971+EmanuelFacundo@users.noreply.github.com>
2021-02-26 15:14:20 -0300

    React Hooks
    Introdução ao react hooks

---
+++ navegacao/src/components/layout/Menu.jsx
@@ -0,0 +1,32 @@
+import './Menu.css'
+
+import React from 'react'
+import { Link } from 'react-router-dom' 
+
+const Menu = props =>{
+    return (
+        <aside className= "Menu"> 
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Início</Link>
+                    </li>
+                    <li>
+                        <Link to="/param/1234">Param #1</Link>
+                    </li>
+                    <li>
+                        <Link to="/param/boa">Param #2</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">Sobre</Link>
+                    </li>
+                    <li>
+                        <Link to="/notfound">Não Existe</Link>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+    )
+}
+
+export default Menu
