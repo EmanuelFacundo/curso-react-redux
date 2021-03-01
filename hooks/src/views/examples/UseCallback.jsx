3aa95e40df3bad3603b4dcb34189ac6c95f0fd11
Emanuel Facundo <38675971+EmanuelFacundo@users.noreply.github.com>
2021-03-01 18:18:29 -0300

    Mais de hooks
    useCallback, useMemo e (useMemo + useCallback)

--- hooks/src/views/examples/UseCallback.jsx
+++ hooks/src/views/examples/UseCallback.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+import UseCallBackButton from "./UseCallbackButtons";
 
 const UseCallback = (props) => {
+
+    const [count, setCount] = useState(0)
+
+    const inc = useCallback(function(delta){
+        setCount(curr => curr + delta)
+    }, [setCount])
+
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
-                subtitle="Retorna uma função memoizada!"
+                subtitle="Retorna uma função memorizada!"
             />
+
+            <SectionTitle tilte="useCallback" />
+            <div className="center">
+                <span className="text">{count}</span>
+                <UseCallBackButton inc={inc} />
+            </div>
         </div>
     )
 }
--- hooks/src/views/examples/UseMemo.jsx
+++ hooks/src/views/examples/UseMemo.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+function sum(a, b){
+    const delay = Date.now() + 1000 //delay de 1s
+    while(Date.now() < delay){}
+
+    return a + b
+}
 
 const UseMemo = (props) => {
+
+    const [n1, setN1] = useState(0)
+    const [n2, setN2] = useState(0)
+    const [n3, setN3] = useState(0)
+
+    const result = useMemo(() => sum(n1,n2), [n1,n2])
+
     return (
         <div className="UseMemo">
             <PageTitle
                 title="Hook UseMemo"
-                subtitle="Retorna um valor memoizado!"
+                subtitle="Retorna um valor memorizado!"
             />
+
+            <SectionTitle title="useMemo()" />
+
+            <div className="center">
+                <input type="number" className="input" value={n1} onChange={(e) => setN1(parseInt(e.target.value))}/>
+                <input type="number" className="input" value={n2} onChange={(e) => setN2(parseInt(e.target.value))}/>
+                <input type="number" className="input" value={n3} onChange={(e) => setN3(parseInt(e.target.value))}/>
+                <span className="text">{result}</span>
+            </div>
         </div>
     )
 }
